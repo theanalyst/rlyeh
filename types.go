0ead296fc6baae090b4548b0b879922f07c48a13
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-09-23 15:00:25 +0530

    types: capture the correct json for journal & ldb

--- types.go
+++ types.go
@@ -5,13 +5,13 @@ type PerfCounter struct {
 		JournalLatency struct {
 			AvgCount int     `json:"avgcount"`
 			Sum      float64 `json:"sum"`
-		}
+		} `json:"journal_latency"`
 		Ops int `json:"ops"`
 	} `json:"filestore"`
 	Leveldb struct {
 		LeveldbGetLatency struct {
 			AvgCount int     `json:"avgcount"`
 			Sum      float64 `json:"sum"`
-		}
+		} `json:"leveldb_get_latency"`
 	} `json:"leveldb"`
 }
