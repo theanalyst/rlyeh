784f8822dc84862a81511a24585a5848e1619658
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-09-30 17:26:31 +0530

    fn to get a cluster handle

--- monitor.go
+++ monitor.go
@@ -5,10 +5,21 @@ import (
 	"github.com/ceph/go-ceph/rados"
 )
 
-func GetClusterStatus(conf string) {
-	conn, _ := rados.NewConn()
+// Return a rados connection handle, given a conf object
+func GetRadosHandle(conf string) (conn *rados.Conn, err error) {
+	conn, err = rados.NewConn()
+	if err != nil {
+		return nil, err
+	}
+
 	conn.ReadConfigFile(conf)
-	conn.Connect()
+
+	err = conn.Connect()
+	return conn, err
+}
+
+func GetClusterStatus(conf string) {
+	conn, err := GetRadosHandle(conf)
 
 	command, err := json.Marshal(map[string]string{"prefix": "report"})
 	buf, _, err := conn.MonCommand(command)
