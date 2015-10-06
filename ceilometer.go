813f845149c502c829ea20bada0de01d3f0cfc97
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-10-06 21:46:09 +0530

    working version of sending stats to ceilometer

--- ceilometer.go
+++ ceilometer.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	log "github.com/Sirupsen/logrus"
 	"github.com/rackspace/gophercloud/openstack"
 	"github.com/rackspace/gophercloud/openstack/identity/v2/tokens"
+	"net/http"
 )
 
 func get_token() (*tokens.Token, error) {
@@ -26,4 +30,27 @@ func get_token() (*tokens.Token, error) {
 		return nil, err
 	}
 	return bearer_token, nil
+}
+
+func PostPerfCeilometer(bearer_token *tokens.Token) error {
+
+	m1 := Meter{
+		Counter_name:   "osd.1.latency",
+		Counter_type:   "gauge",
+		Resource_id:    "1",
+		Counter_unit:   "ms",
+		Counter_volume: 10.665}
+
+	meter := []Meter{m1}
+	log.Debug("Meter json")
+
+	m, err := json.Marshal(meter)
+	log.Debug(string(m))
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "http://192.168.122.178:8777/v2/meters/osd.1.latency", bytes.NewBuffer(m))
+	req.Header.Add("X-Auth-Token", bearer_token.ID)
+	req.Header.Add("content-type", "application/json")
+	resp, err := client.Do(req)
+	log.Debug(resp)
+	return err
 }
